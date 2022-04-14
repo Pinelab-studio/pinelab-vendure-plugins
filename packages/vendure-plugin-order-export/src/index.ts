d7b1eb3aafd5344e8fbe487fff3d65b77536c2b5
Martijn <martijn.brug@gmail.com>
2022-04-14 12:47:56 +0200

    feat: order-export

--- packages/vendure-plugin-order-export/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { PermissionDefinition } from '@vendure/core';
-
-export const orderExportPermission = new PermissionDefinition({
-  name: 'ExportOrders',
-  description: 'Allows exporting orders via order-export plugin',
-});
-export * from './order-export.plugin';
-export * from './api/order-export-strategy';
-export * from './ui/generated/graphql';
