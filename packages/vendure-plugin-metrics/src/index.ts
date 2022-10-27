7e9c6a2401e21815adb2e686c9b71621b51946ce
Martijn <martijn.brug@gmail.com>
2022-10-27 10:23:56 +0200

    fix(metrics): export more types

--- packages/vendure-plugin-metrics/src/index.ts
+++ packages/vendure-plugin-metrics/src/index.ts
@@ -1,3 +1,5 @@
 export * from './MetricsPlugin';
 export * from './api/strategies';
+export * from './api/metrics.service';
+export * from './api/metrics.resolver';
 export * from './ui/generated/graphql';
