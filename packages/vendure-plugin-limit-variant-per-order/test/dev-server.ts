1a370cb730a2dbbb8925d300b85ee54731d98f45
Martijn <martijn.brug@gmail.com>
2022-12-19 05:16:54 +0100

    feat(limit-variant): finished plugin

---
+++ packages/vendure-plugin-limit-variant-per-order/test/dev-server.ts
@@ -0,0 +1,54 @@
+import { initialData } from '../../test/src/initial-data';
+import {
+  createTestEnvironment,
+  registerInitializer,
+  SqljsInitializer,
+  testConfig,
+} from '@vendure/testing';
+import {
+  ChannelService,
+  DefaultLogger,
+  DefaultSearchPlugin,
+  LogLevel,
+  mergeConfig,
+} from '@vendure/core';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import { testPaymentMethod } from '../../test/src/test-payment-method';
+import { LimitVariantPerOrderPlugin } from '../src/limit-variant-per-order.plugin';
+
+require('dotenv').config();
+
+(async () => {
+  registerInitializer('sqljs', new SqljsInitializer('__data__'));
+  const devConfig = mergeConfig(testConfig, {
+    logger: new DefaultLogger({ level: LogLevel.Debug }),
+    plugins: [
+      LimitVariantPerOrderPlugin,
+      DefaultSearchPlugin,
+      AdminUiPlugin.init({
+        port: 3002,
+        route: 'admin',
+      }),
+    ],
+    apiOptions: {
+      shopApiPlayground: true,
+    },
+    paymentOptions: {
+      paymentMethodHandlers: [testPaymentMethod],
+    },
+  });
+  const { server, adminClient, shopClient } = createTestEnvironment(devConfig);
+  await server.init({
+    initialData: {
+      ...initialData,
+      paymentMethods: [
+        {
+          name: testPaymentMethod.code,
+          handler: { code: testPaymentMethod.code, arguments: [] },
+        },
+      ],
+    },
+    productsCsvPath: '../test/src/products-import.csv',
+  });
+  const channel = await server.app.get(ChannelService).getDefaultChannel();
+})();
