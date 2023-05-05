7a9dfe1734684ebf087df4c9167abccecc39db35
Martijn <martijn.brug@gmail.com>
2023-05-05 09:41:26 +0200

    feat(metrics): more typos

--- packages/vendure-plugin-metrics/src/constants.ts
+++ packages/vendure-plugin-metrics/src/constants.ts
@@ -1,3 +1,2 @@
-export const loggerCtx = 'SalesPerVariantPlugin';
-export const PLUGIN_INIT_OPTIONS = Symbol('SALES_PER_VARIANT_PLUGIN_OPTIONS');
-export const PLUGIN_NAME = 'vendure-plugin-sales-per-variant';
+export const loggerCtx = 'MetricsPlugin';
+export const PLUGIN_INIT_OPTIONS = Symbol('PLUGIN_INIT_OPTIONS');
