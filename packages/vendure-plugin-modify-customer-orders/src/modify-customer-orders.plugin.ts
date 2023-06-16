c41fd07da1919ba1ff09e945ab25e8ee7404aa0a
Surafel Melese <surafelmelese09@gmail.com>
2023-06-16 11:33:32 +0300

    feat: some 'as any' need to be added to use util functions from test package

--- packages/vendure-plugin-modify-customer-orders/src/modify-customer-orders.plugin.ts
+++ packages/vendure-plugin-modify-customer-orders/src/modify-customer-orders.plugin.ts
@@ -18,7 +18,7 @@ export interface ModifyCustomerOrdersPluginOptions {
   imports: [PluginCommonModule],
   adminApiExtensions: {
     resolvers: [AdminApiResolver],
-    schema: adminApiExtension,
+    schema: adminApiExtension as any,
   },
   providers: [
     OrderTransitionListenerService,
