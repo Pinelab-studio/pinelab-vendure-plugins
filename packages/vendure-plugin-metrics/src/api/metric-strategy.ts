456888f740c5e60ae3e4e2e8a661ce417f8ceb58
Martijn <martijn.brug@gmail.com>
2023-08-02 14:36:33 +0200

    feat(metrics): example interface

--- packages/vendure-plugin-metrics/src/api/metric-strategy.ts
+++ packages/vendure-plugin-metrics/src/api/metric-strategy.ts
@@ -1,36 +1,81 @@
-import { AdvancedMetricInterval, AdvancedMetricSummaryEntry, AdvancedMetricType } from "../ui/generated/graphql";
-import { RequestContext } from '@vendure/core';
-import { MetricData } from "./metrics.service";
+import {
+  AdvancedMetricInterval,
+  AdvancedMetricSummaryEntry,
+  AdvancedMetricType,
+} from '../ui/generated/graphql';
+import { RequestContext, Injector } from '@vendure/core';
 
-export interface MetricStrategy<T extends Array> {
-    code: string;
-    /**
-     * We need to know if the chart should format your metrics as
-     *  numbers/amounts or as currency
-     */
-    metricType: AdvancedMetricType;
+type DataWithCreatedAt = { createdAt: Date };
+export interface MetricStrategy<T extends DataWithCreatedAt[]> {
+  code: string;
+  /**
+   * We need to know if the chart should format your metrics as
+   *  numbers/amounts or as currency
+   */
+  metricType: AdvancedMetricType;
 
-    /**
-     * Title to display on the chart. 
-     * Ctx can be used to localize the title
-     */
-    getTitle(ctx: RequestContext): string;
+  /**
+   * Title to display on the chart.
+   * Ctx can be used to localize the title
+   */
+  getTitle(ctx: RequestContext): string;
 
-    loadData(ctx: RequestContext)
+  /**
+   * Load your data for the given time frame here.
+   *
+   * Keep performance and object size in mind:
+   *
+   * Data is cached in memory, so only return data you actually use in your calculateDataPoint function
+   *
+   * This function is executed in the main thread when a user views its dashboard,
+   * so try not to fetch objects with many relations
+   */
+  loadData(
+    ctx: RequestContext,
+    injector: Injector,
+    from: Date,
+    to: Date
+  ): Promise<T>;
 
-    /**
-     * Calculate the datapoint for the given month
-     * @param ctx 
-     * @param interval 
-     * @param monthNr 
-     * @param data 
-     */
-    calculateDataPoint(
-        ctx: RequestContext,
-        monthNr: number,
-        data: T
-      ): AdvancedMetricSummaryEntry;
-    
+  /**
+   * Calculate the datapoint for the given month
+   *
+   * @param ctx
+   * @param data The data for the current timeframe
+   */
+  calculateDataPoint(ctx: RequestContext, data: T, monthNr: number): number;
+}
 
+// FIXME Sample strategy for Stripe subscription. Remove later
+type StripePayment = { amount: number; createdAt: Date };
 
+export class StripeSubscriptionMetric
+  implements MetricStrategy<StripePayment[]>
+{
+  code = 'stripe-subscription-payment';
+  metricType = AdvancedMetricType.Currency;
+
+  getTitle(ctx: RequestContext): string {
+    return `Received payments`;
+  }
+
+  async loadData(
+    ctx: RequestContext,
+    injector: Injector,
+    from: Date,
+    to: Date
+  ): Promise<StripePayment[]> {
+    // Load payments between `from` and `to`
+    return [
+      {
+        amount: 123,
+        createdAt: new Date(),
+      },
+    ];
+  }
+
+  calculateDataPoint(ctx: RequestContext, data: StripePayment[]): number {
+    // Calculate the sum of all payments for this month
+    return data.reduce((acc, curr) => acc + curr.amount, 0);
+  }
 }
