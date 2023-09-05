4958cc93b429599401ddc7efd601450ad839633b
Martijn <martijn.brug@gmail.com>
2023-09-05 08:49:59 +0200

    docs: docs website wip

---
+++ docs-website/src/api/plugins.ts
@@ -0,0 +1,36 @@
+import { readdir, readFile } from 'fs/promises';
+import type { Dirent } from 'node:fs';
+import path from 'path';
+
+interface PackageJson {
+  name: string;
+  description: string;
+  versione: string;
+  // TODO icon\
+}
+
+const pluginDirName = '../packages/';
+
+export async function getPluginDirectories(): Promise<Dirent[]> {
+  return (await readdir(pluginDirName, { withFileTypes: true }))
+    .filter((dir) => dir.isDirectory())
+    .filter((dir) => dir.name.startsWith('vendure-plugin'));
+}
+
+export async function getPackageJsons(): Promise<PackageJson[]> {
+  const pluginDirectories = await getPluginDirectories();
+  const allPackageDotJsonPaths = pluginDirectories.map((r) =>
+    path.join(pluginDirName, r.name, 'package.json')
+  );
+  return await Promise.all(
+    allPackageDotJsonPaths
+      .map(async (p) => {
+        try {
+          return JSON.parse(await readFile(p, 'utf8'));
+        } catch (e) {
+          throw Error(`Unable to parse "${p}"`);
+        }
+      })
+      .filter((v: any) => v !== undefined)
+  );
+}
