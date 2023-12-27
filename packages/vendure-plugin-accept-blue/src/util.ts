e766878e95fc66d670f0e51006983945195ea166
Martijn <martijn.brug@gmail.com>
2023-12-27 13:20:43 +0100

    feat(accept-blue): wip

---
+++ packages/vendure-plugin-accept-blue/src/util.ts
@@ -0,0 +1,19 @@
+interface CardInput {
+  card: string;
+  expiry_month: number;
+  expiry_year: number;
+}
+
+interface CardToCheck {
+  last4: string;
+  expiry_month: number;
+  expiry_year: number;
+}
+
+export function isSameCard(input: CardInput, card: CardToCheck): boolean {
+  return (
+    input.card === card.last4 &&
+    input.expiry_month === card.expiry_month &&
+    input.expiry_year === card.expiry_year
+  );
+}
---
+++ packages/vendure-plugin-accept-blue/test/e2e.spec.ts
@@ -0,0 +1,1 @@
+// TODO test if getPaymentMethods only works for logged in users. IMPORTANT
