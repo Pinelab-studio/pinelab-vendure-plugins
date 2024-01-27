acfcc592b8251007b7cdab99d31de805fd508271
Surafel <surafel@Surafels-MacBook-Air.local>
2024-01-27 22:50:23 +0300

    feat: added regenerate invocies button

--- packages/vendure-plugin-invoices/src/ui/invoices-nav.module.ts
+++ packages/vendure-plugin-invoices/src/ui/invoices-nav.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { addNavMenuItem, SharedModule } from '@vendure/admin-ui/core';
+import {
+  addNavMenuItem,
+  SharedModule,
+  addActionBarItem,
+} from '@vendure/admin-ui/core';
 import { registerCustomDetailComponent } from '@vendure/admin-ui/core';
+
 import { InvoiceDetailViewComponent } from './invoices-detail-view/invoices-detail-view.component';
+import { createInvoice } from './queries.graphql';
+import { catchError } from 'rxjs/operators';
+import { ApolloCache } from '@apollo/client/cache';
 
 @NgModule({
   imports: [SharedModule],
@@ -19,6 +27,40 @@ import { InvoiceDetailViewComponent } from './invoices-detail-view/invoices-deta
     registerCustomDetailComponent({
       locationId: 'order-detail',
       component: InvoiceDetailViewComponent,
+    }),
+    addActionBarItem({
+      id: 'regenerate-invoice',
+      label: 'Regenerate Invoice',
+      locationId: 'order-detail',
+      onClick: (event, context) => {
+        const orderId = context.route.snapshot.params['id'];
+        (event.target as HTMLButtonElement).disabled = true;
+        context.dataService
+          .mutate(createInvoice, { orderId }, (cache: ApolloCache<any>, _) => {
+            cache.evict({ fieldName: 'order' });
+          })
+          .pipe(
+            catchError((error) => {
+              context.notificationService.error(
+                'Failed to regenerate invoices'
+              );
+              (event.target as HTMLButtonElement).disabled = false;
+              throw new Error('An error occurred. Please try again later.');
+            })
+          )
+          .subscribe((data: any) => {
+            if (data?.createInvoice?.id) {
+              context.notificationService.success(
+                'Invoice has been regenerated successfully'
+              );
+            } else {
+              context.notificationService.error(
+                'Failed to regenerate invoices'
+              );
+            }
+            (event.target as HTMLButtonElement).disabled = false;
+          });
+      },
     }),
   ],
 })
