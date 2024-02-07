779c461db1f03577b7abdec8fac06e9a0b0cb374
Martijn <martijn.brug@gmail.com>
2024-02-07 11:30:47 +0100

    feat(invoices): e2e testing

--- packages/vendure-plugin-invoices/test/e2e.spec.ts
+++ packages/vendure-plugin-invoices/test/e2e.spec.ts
@@ -215,6 +215,7 @@ describe('Generate with credit invoicing enabled', function () {
       id: order.id,
     });
     const invoices: Invoice[] = result.invoices;
+    console.log(JSON.stringify(invoices, null, 2));
     // Latest invoice
     expect(invoices.length).toBe(3);
     expect(invoices[2].invoiceNumber).toBe(1);
