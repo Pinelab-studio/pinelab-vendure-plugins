b17f5b3ab1af1df7ff8b595cd40954744babb630
Martijn <martijn.brug@gmail.com>
2024-02-19 15:59:32 +0100

    feat: new linting setup

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,7 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  singleQuote: true,
   plugins: ["./node_modules/prettier-plugin-astro/dist/index.js"],
   overrides: [
     {
