6290887c98ab1bc91983bccce39ed017fb124788
Martijn <martijn.brug@gmail.com>
2024-04-02 13:35:35 +0200

    feat: eslint config setup

---
+++ eslint-base-config.js
@@ -0,0 +1,14 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+// This config acts as base for all configs inside packages/*
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  // Prevent formatting conflicts between eslint and Prettier. Always apply last
+  eslintConfigPrettier,
+  {
+    ignores: ['*.config.js', 'dist', 'node_modules', '**/*/dev-server.ts'],
+  }
+);
