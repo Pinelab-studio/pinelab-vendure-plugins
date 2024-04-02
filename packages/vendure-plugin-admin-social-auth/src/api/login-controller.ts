9836487fbebd183c9e99006aa018b2232c322215
Martijn <martijn.brug@gmail.com>
2024-04-02 14:57:29 +0200

    feat(admin-social-auth): fixed linting errors

--- packages/vendure-plugin-admin-social-auth/src/api/login-controller.ts
+++ packages/vendure-plugin-admin-social-auth/src/api/login-controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get, Inject, Res } from '@nestjs/common';
-import { ConfigService, VENDURE_VERSION, Injector } from '@vendure/core';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import { AdminUiConfig } from '@vendure/common/lib/shared-types';
+import { ConfigService, VENDURE_VERSION } from '@vendure/core';
 import { Response } from 'express';
 import fs from 'fs/promises';
 import Handlebars from 'handlebars';
-import { PLUGIN_INIT_OPTIONS } from '../constants';
 import { AdminSocialAuthPluginOptions } from '../admin-social-auth.plugin';
-import { AdminUiConfig } from '@vendure/common/lib/shared-types';
-import { ModuleRef } from '@nestjs/core';
-import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import { PLUGIN_INIT_OPTIONS } from '../constants';
 
 /**
  * Custom login page
@@ -17,16 +16,16 @@ export class LoginController {
   constructor(
     @Inject(PLUGIN_INIT_OPTIONS)
     private readonly options: AdminSocialAuthPluginOptions,
-    private readonly configService: ConfigService,
-    private readonly moduleRef: ModuleRef
+    private readonly configService: ConfigService
   ) {}
 
   @Get('social-auth/login')
   async getLogin(@Res() res: Response): Promise<void> {
-    // Dirty hack to get the private static adminUiConfig
+    /* eslint-disable -- Dirty hack to get the private static adminUiConfig*/
     const adminUiConfig: Partial<AdminUiConfig> | undefined = (
       AdminUiPlugin as any
     ).options?.adminUiConfig;
+    /* eslint-enable */
     const loginHtml = await fs.readFile(
       `${__dirname}/../ui/login.html`,
       'utf8'
