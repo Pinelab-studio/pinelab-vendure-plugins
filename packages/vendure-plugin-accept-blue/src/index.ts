d8a582e4f0a95456ba357e52b095405ddb0e4a18
Martijn <martijn.brug@gmail.com>
2024-07-11 14:32:18 +0200

    feat(accept-blue): unused strategy

--- packages/vendure-plugin-accept-blue/src/index.ts
+++ packages/vendure-plugin-accept-blue/src/index.ts
@@ -1,5 +1,4 @@
 export * from '../../util/src/subscription/default-subscription-strategy';
-export * from '../../util/src/subscription/webhook-subscription-strategy';
 export * from '../../util/src/subscription/subscription-strategy';
 export * from '../../util/src/subscription/subscription-helper';
 export * from './accept-blue-plugin';
