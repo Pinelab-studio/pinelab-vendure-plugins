16e52fb1e57f6b816ece93e686424c944156f4a4
Martijn <martijn.brug@gmail.com>
2024-07-16 09:57:29 +0200

    feat(vendure-scripts): prettier

--- packages/vendure-scripts/src/copy-database.ts
+++ packages/vendure-scripts/src/copy-database.ts
@@ -1,40 +1,50 @@
-const Confirm = require("prompt-confirm");
-import { spawn } from "child_process"
+const Confirm = require('prompt-confirm');
+import { spawn } from 'child_process';
 
 export interface DBConfig {
-    username: string;
-    password: string;
-    host: string;
-    databaseName: string;
+  username: string;
+  password: string;
+  host: string;
+  databaseName: string;
 }
 
 /**
  * Download prod db to local file with mysqldump
  */
-export async function exportDbToFile(config: DBConfig, exportFile = `/tmp/db_export.sql`): Promise<string> {
-    const promptCommand = new Confirm(
-        `Downloading database ${config.databaseName} (${config.host}) to "${exportFile}". Proceed?`,
-    );
-    const accept = await promptCommand.run();
-    if (!accept) {
-        process.exit(0);
-    }
-    await execute(`mysqldump -v --set-gtid-purged=OFF --column-statistics=0 -u ${config.username} -h ${config.host} --password='${config.password}' ${config.databaseName} > ${exportFile}`);
-    return exportFile;
+export async function exportDbToFile(
+  config: DBConfig,
+  exportFile = `/tmp/db_export.sql`
+): Promise<string> {
+  const promptCommand = new Confirm(
+    `Downloading database ${config.databaseName} (${config.host}) to "${exportFile}". Proceed?`
+  );
+  const accept = await promptCommand.run();
+  if (!accept) {
+    process.exit(0);
+  }
+  await execute(
+    `mysqldump -v --set-gtid-purged=OFF --column-statistics=0 -u ${config.username} -h ${config.host} --password='${config.password}' ${config.databaseName} > ${exportFile}`
+  );
+  return exportFile;
 }
 
 /**
  * Import the given sql file into the database
  */
-export async function insertIntoDb(config: DBConfig, sqlFile: string): Promise<void> {
-    const promptCommand = new Confirm(
-        `Inserting "${sqlFile}" into database ${config.databaseName} (${config.host}). Proceed?`,
-    );
-    const accept = await promptCommand.run();
-    if (!accept) {
-        process.exit(0);
-    }
-    await execute(`mysql -u ${config.username} -h ${config.host} --password='${config.password}' ${config.databaseName} < ${sqlFile}`);
+export async function insertIntoDb(
+  config: DBConfig,
+  sqlFile: string
+): Promise<void> {
+  const promptCommand = new Confirm(
+    `Inserting "${sqlFile}" into database ${config.databaseName} (${config.host}). Proceed?`
+  );
+  const accept = await promptCommand.run();
+  if (!accept) {
+    process.exit(0);
+  }
+  await execute(
+    `mysql -u ${config.username} -h ${config.host} --password='${config.password}' ${config.databaseName} < ${sqlFile}`
+  );
 }
 
 /**
@@ -42,17 +52,17 @@ export async function insertIntoDb(config: DBConfig, sqlFile: string): Promise<v
  * Logs the output of the command to the console in realtime.
  */
 async function execute(command: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const proc = spawn(command, [], { stdio: "inherit", shell: true });
-        proc.on("error", function (error: any) {
-            reject(new Error(error.message));
-        });
-        proc.on("exit", function (code: any) {
-            if (code !== 0) {
-                reject(new Error("exited with code " + code));
-            } else {
-                resolve();
-            }
-        });
+  return new Promise((resolve, reject) => {
+    const proc = spawn(command, [], { stdio: 'inherit', shell: true });
+    proc.on('error', function (error: any) {
+      reject(new Error(error.message));
     });
+    proc.on('exit', function (code: any) {
+      if (code !== 0) {
+        reject(new Error('exited with code ' + code));
+      } else {
+        resolve();
+      }
+    });
+  });
 }
