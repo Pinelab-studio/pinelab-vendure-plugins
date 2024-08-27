fae306390a6f0e74f5924392dfd61fd15243b334
Surafel <surafel@Surafels-MacBook-Air.local>
2024-08-27 12:27:49 +0300

    feat: try/catch added in controller

--- packages/vendure-plugin-shipmate/src/api/shipmate.controller.ts
+++ packages/vendure-plugin-shipmate/src/api/shipmate.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { EventPayload } from '../types';
 import { ShipmateService } from './shipmate.service';
-import { RequestContext, Ctx } from '@vendure/core';
+import { RequestContext, Ctx, Logger } from '@vendure/core';
+import { asError } from 'catch-unknown';
 
 @Controller('shipmate')
 export class ShipmateController {
   constructor(private shipmentService: ShipmateService) {}
   @Post()
   async event(@Ctx() ctx: RequestContext, @Body() payload: EventPayload) {
-    await this.shipmentService.updateOrderState(payload);
+    try {
+      await this.shipmentService.updateOrderState(payload);
+    } catch (e) {
+      Logger.error(
+        `Shipmate Order state update failed with the message: ${
+          asError(e).message
+        }`
+      );
+    }
   }
 }
