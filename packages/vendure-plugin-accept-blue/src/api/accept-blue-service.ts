e6829a7b484ebbf358d36be9333bf9f3a1e54bac
mschipperheyn <m.schipperheyn@gmail.com>
2024-09-02 15:23:39 -0300

    fix(accept-blue): make handler.code optional to avoid server crash

--- packages/vendure-plugin-accept-blue/src/api/accept-blue-service.ts
+++ packages/vendure-plugin-accept-blue/src/api/accept-blue-service.ts
@@ -80,7 +80,7 @@ export class AcceptBlueService implements OnApplicationBootstrap {
       .ofType(PaymentMethodEvent)
       .pipe(
         filter(
-          (data) => data.entity.handler.code === acceptBluePaymentHandler.code
+          (data) => data.entity.handler?.code === acceptBluePaymentHandler.code
         )
       )
       .subscribe(({ ctx, entity }) => {
