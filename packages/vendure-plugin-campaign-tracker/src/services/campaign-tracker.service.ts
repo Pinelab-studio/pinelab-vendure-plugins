5843b057f9c28369f27d1f2f4d91b83bd454dab0
Martijn <martijn.brug@gmail.com>
2024-10-21 15:20:24 +0200

    feat: wip campaing tracking

--- packages/vendure-plugin-campaign-tracker/src/services/campaign-tracker.service.ts
+++ packages/vendure-plugin-campaign-tracker/src/services/campaign-tracker.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { JobState } from '@vendure/common/lib/generated-types';
 import {
   ID,
@@ -14,12 +14,13 @@ import { Campaign } from '../entities/campaign.entity';
 import { PluginInitOptions } from '../types';
 import { CampaignInput } from '../ui/generated/graphql';
 
+interface JobData {
+  ctx: SerializedRequestContext;
+}
+
 @Injectable()
 export class CampaignTrackerService implements OnModuleInit {
-  private calculateCampaignMetricsQueue: JobQueue<{
-    ctx: SerializedRequestContext;
-    someArg: string;
-  }>;
+  private jobQueue!: JobQueue<JobData>;
 
   constructor(
     private connection: TransactionalConnection,
@@ -28,42 +29,16 @@ export class CampaignTrackerService implements OnModuleInit {
   ) {}
 
   public async onModuleInit(): Promise<void> {
-    this.calculateCampaignMetricsQueue = await this.jobQueueService.createQueue(
-      {
-        name: 'calculate-campaign-metrics',
-        process: async (job) => {
-          // Deserialize the RequestContext from the job data
-          const ctx = RequestContext.deserialize(job.data.ctx);
-          // The "someArg" property is passed in when the job is triggered
-          const someArg = job.data.someArg;
-
-          // Inside the `process` function we define how each job
-          // in the queue will be processed.
-          // Let's simulate some long-running task
-          const totalItems = 10;
-          for (let i = 0; i < totalItems; i++) {
-            await new Promise((resolve) => setTimeout(resolve, 500));
-
-            // You can optionally respond to the job being cancelled
-            // during processing. This can be useful for very long-running
-            // tasks which can be cancelled by the user.
-            if (job.state === JobState.CANCELLED) {
-              throw new Error('Job was cancelled');
-            }
-
-            // Progress can be reported as a percentage like this
-            job.setProgress(Math.floor((i / totalItems) * 100));
-          }
-
-          // The value returned from the `process` function is stored
-          // as the "result" field of the job
-          return {
-            processedCount: totalItems,
-            message: `Successfully processed ${totalItems} items`,
-          };
-        },
-      }
-    );
+    this.jobQueue = await this.jobQueueService.createQueue({
+      name: 'campaign-tracker',
+      process: (job) => {
+        this.calculateCampaignMetrics(
+          RequestContext.deserialize(job.data.ctx)
+        ).catch((err) => {
+          Logger.warn(`Error in calculateCampaignMetrics: ${err?.message}`);
+        });
+      },
+    });
   }
 
   async createCampaign(
@@ -71,10 +46,9 @@ export class CampaignTrackerService implements OnModuleInit {
     input: CampaignInput
   ): Promise<Campaign> {}
 
-  public triggerCalculateCampaignMetrics(ctx: RequestContext) {
-    return this.calculateCampaignMetricsQueue.add({
+  async triggerCalculateCampaignMetrics(ctx: RequestContext): Promise<void> {
+    await this.jobQueue.add({
       ctx: ctx.serialize(),
-      someArg: 'foo',
     });
   }
 
