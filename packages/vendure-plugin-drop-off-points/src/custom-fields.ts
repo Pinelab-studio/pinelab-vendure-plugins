aabb3f82f6a72aa4fecac3a5ff28d338fe11251d
Martijn <martijn.brug@gmail.com>
2025-02-04 11:28:25 +0100

    feat(drop-off-points): pr improvements

--- packages/vendure-plugin-drop-off-points/src/custom-fields.ts
+++ packages/vendure-plugin-drop-off-points/src/custom-fields.ts
@@ -6,7 +6,7 @@ export const customOrderFields: CustomFieldConfig[] = [
   {
     name: 'dropOffPointCarrier',
     type: 'string',
-    label: [{ value: 'Drop Off Point Carrier', languageCode: LanguageCode.en }],
+    label: [{ value: 'Carrier', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
   {
@@ -18,31 +18,25 @@ export const customOrderFields: CustomFieldConfig[] = [
   {
     name: 'dropOffPointName',
     type: 'string',
-    label: [{ value: 'Drop Off Point Name', languageCode: LanguageCode.en }],
+    label: [{ value: 'Name', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
   {
     name: 'dropOffPointStreetLine1',
     type: 'string',
-    label: [
-      { value: 'Drop Off Point Street Line 1', languageCode: LanguageCode.en },
-    ],
+    label: [{ value: 'Street Line 1', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
   {
     name: 'dropOffPointStreetLine2',
     type: 'string',
-    label: [
-      { value: 'Drop Off Point Street Line 2', languageCode: LanguageCode.en },
-    ],
+    label: [{ value: 'Street Line 2', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
   {
     name: 'dropOffPointHouseNumber',
     type: 'string',
-    label: [
-      { value: 'Drop Off Point House Number', languageCode: LanguageCode.en },
-    ],
+    label: [{ value: 'House Number', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
   {
@@ -50,7 +44,7 @@ export const customOrderFields: CustomFieldConfig[] = [
     type: 'string',
     label: [
       {
-        value: 'Drop Off Point House Number Suffix',
+        value: 'House Number Suffix',
         languageCode: LanguageCode.en,
       },
     ],
@@ -59,21 +53,19 @@ export const customOrderFields: CustomFieldConfig[] = [
   {
     name: 'dropOffPointPostalCode',
     type: 'string',
-    label: [
-      { value: 'Drop Off Point Postal Code', languageCode: LanguageCode.en },
-    ],
+    label: [{ value: 'Postal Code', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
   {
     name: 'dropOffPointCity',
     type: 'string',
-    label: [{ value: 'Drop Off Point City', languageCode: LanguageCode.en }],
+    label: [{ value: 'City', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
   {
     name: 'dropOffPointCountry',
     type: 'string',
-    label: [{ value: 'Drop Off Point Country', languageCode: LanguageCode.en }],
+    label: [{ value: 'Country', languageCode: LanguageCode.en }],
     ui: { tab: uiTab },
   },
 ];
