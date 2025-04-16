cfa513e38d9ae9170b7a148f813e90c5b0793d2d
Martijn <martijn.brug@gmail.com>
2025-04-16 12:46:35 +0200

    fix(invoices,frequently-bought-together): piad plugin notifications

--- packages/vendure-plugin-picqer/src/api/picqer.service.ts
+++ packages/vendure-plugin-picqer/src/api/picqer.service.ts
@@ -406,8 +406,8 @@ export class PicqerService implements OnApplicationBootstrap {
         await this.transitionToState(ctx, order, 'Shipped');
         await this.transitionToState(ctx, order, 'Delivered');
       } catch (e) {
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
         Logger.error(
+          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
           `Failed to transition order ${order.code}: ${e}`,
           loggerCtx,
           util.inspect(e)
