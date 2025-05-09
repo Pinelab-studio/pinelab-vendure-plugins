291b09671c0c96ba820c43d105056ec8ab77072d
Martijn <martijn.brug@gmail.com>
2025-05-09 10:44:40 +0200

    feat(promotion-extensions): reverted addreess lookup changes

--- packages/vendure-plugin-address-lookup/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ADDRESS_LOOKUP_PLUGIN_OPTIONS = Symbol(
-  'ADDRESS_LOOKUP_PLUGIN_OPTIONS'
-);
-export const loggerCtx = 'AddressLookupPlugin';
--- packages/vendure-plugin-address-lookup/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './address-lookup.plugin';
-export * from './types';
-export * from './generated/graphql';
-export * from './config/post-nl-lookup-strategy';
-export * from './config/postcode-tech-strategy';
