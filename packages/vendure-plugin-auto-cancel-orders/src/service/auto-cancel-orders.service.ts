414986d3929d831038bbd7925231ca0963a6d3d6
Martijn <martijn.brug@gmail.com>
2025-05-14 16:59:54 +0200

    feat: cancel orders

--- packages/vendure-plugin-auto-cancel-orders/src/service/auto-cancel-orders.service.ts
+++ packages/vendure-plugin-auto-cancel-orders/src/service/auto-cancel-orders.service.ts
@@ -8,9 +8,11 @@ import {
   ID,
   TransactionalConnection,
   SerializedRequestContext,
+  Logger,
 } from '@vendure/core';
-import { AUTO_CANCEL_ORDERS_OPTIONS } from '../constants';
+import { AUTO_CANCEL_ORDERS_OPTIONS, loggerCtx } from '../constants';
 import { AutoCancelOrdersOptions } from '../auto-cancel-orders.plugin';
+import { LessThan } from 'typeorm';
 
 @Injectable()
 export class AutoCancelOrdersService implements OnModuleInit {
@@ -47,15 +49,35 @@ export class AutoCancelOrdersService implements OnModuleInit {
    * Cancel all active orders that have not been updated in the given number of days
    */
   async cancelActiveOrders(ctx: RequestContext, olderThanDays: number) {
-    // find orders older than the given number of days
+    // Find orders older than the given number of days
+    const olderThanDate = new Date(
+      Date.now() - olderThanDays * 24 * 60 * 60 * 1000
+    );
     let hasMore = true;
+    Logger.info(
+      `Cancelling active orders older than ${olderThanDays} days`,
+      loggerCtx
+    );
     while (hasMore) {
       const orders = await this.connection.getRepository(ctx, Order).find({
         where: {
           active: true,
-          updated: LessThan(olderThanDays),
+          updatedAt: LessThan(olderThanDate),
         },
+        order: {
+          updatedAt: 'ASC',
+        },
+        take: 100,
       });
+      Logger.info(
+        `Cancelling ${orders.length} orders from ${orders[0].updatedAt} to ${
+          orders[orders.length - 1].updatedAt
+        }`,
+        loggerCtx
+      );
+      for (const order of orders) {
+        await this.orderService.cancelOrder(ctx, order.id);
+      }
     }
   }
 }
