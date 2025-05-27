5183a091073ea46601d7d3575552c171dc2f0dde
Martijn <martijn.brug@gmail.com>
2025-05-27 10:53:15 +0200

    fix(accept-blue): revert dev-server

--- packages/vendure-plugin-accept-blue/test/dev-server.ts
+++ packages/vendure-plugin-accept-blue/test/dev-server.ts
@@ -7,7 +7,6 @@ import {
   LanguageCode,
   LogLevel,
   mergeConfig,
-  RequestContextService,
   VendureConfig,
 } from '@vendure/core';
 import {
@@ -16,7 +15,7 @@ import {
   SqljsInitializer,
 } from '@vendure/testing';
 import readline from 'readline';
-import { AcceptBluePlugin, AcceptBlueService } from '../src';
+import { AcceptBluePlugin } from '../src';
 import { acceptBluePaymentHandler } from '../src/api/accept-blue-handler';
 import { AcceptBlueTestCheckoutPlugin } from './helpers/accept-blue-test-checkout.plugin';
 import {
@@ -33,7 +32,6 @@ import { add } from 'date-fns';
 import { TestSubscriptionStrategy } from './helpers/test-subscription-strategy';
 import { SetShippingAddress } from '../../test/src/generated/shop-graphql';
 import { testPaymentMethod } from '../../test/src/test-payment-method';
-import { appendFile } from 'fs';
 
 /**
  * Ensure you have a .env in the plugin root directory with the variable ACCEPT_BLUE_TOKENIZATION_SOURCE_KEY=pk-abc123
@@ -135,84 +133,75 @@ import { appendFile } from 'fs';
   });
   console.log(`Created paymentMethod`);
 
-  // Create CTX
-  const ctx = await server.app.get(RequestContextService).create({
-    apiType: 'admin',
+  // Prepare sample order
+  await shopClient.asUserWithCredentials('hayden.zieme12@hotmail.com', 'test');
+  await shopClient.query(ADD_ITEM_TO_ORDER, {
+    productVariantId: '3',
+    quantity: 1,
   });
-  const client = await server.app
-    .get(AcceptBlueService)
-    .getClientForChannel(ctx);
-  console.log(await client.getCustomer('jeff_ka8+sharong@hotmail.com'));
+  console.log(`Added item`);
+  await shopClient.query(SetShippingAddress, {
+    input: {
+      fullName: 'Hayden Shipping Name',
+      streetLine1: 'Hayden Shipping Street 1',
+      streetLine2: 'Hayden Shipping Street 2',
+      city: 'City of Hayden',
+      postalCode: '1234 XX',
+      countryCode: 'US',
+    },
+  });
+  await shopClient.query(SET_SHIPPING_METHOD, {
+    id: [1],
+  });
+  console.log(`Shipping method set`);
+  const { transitionOrderToState } = await shopClient.query(
+    TRANSITION_ORDER_TO,
+    {
+      state: 'ArrangingPayment',
+    }
+  );
+  console.log(
+    `Transitioned order '${transitionOrderToState.code}' to ArrangingPayment`
+  );
 
-  // // Prepare sample order
-  // await shopClient.asUserWithCredentials('hayden.zieme12@hotmail.com', 'test');
-  // await shopClient.query(ADD_ITEM_TO_ORDER, {
-  //   productVariantId: '3',
-  //   quantity: 1,
-  // });
-  // console.log(`Added item`);
-  // await shopClient.query(SetShippingAddress, {
-  //   input: {
-  //     fullName: 'Hayden Shipping Name',
-  //     streetLine1: 'Hayden Shipping Street 1',
-  //     streetLine2: 'Hayden Shipping Street 2',
-  //     city: 'City of Hayden',
-  //     postalCode: '1234 XX',
-  //     countryCode: 'US',
-  //   },
-  // });
-  // await shopClient.query(SET_SHIPPING_METHOD, {
-  //   id: [1],
-  // });
-  // console.log(`Shipping method set`);
-  // const { transitionOrderToState } = await shopClient.query(
-  //   TRANSITION_ORDER_TO,
-  //   {
-  //     state: 'ArrangingPayment',
-  //   }
-  // );
-  // console.log(
-  //   `Transitioned order '${transitionOrderToState.code}' to ArrangingPayment`
-  // );
-
-  // // Add payment
-  // // Use this metadata in AddpaymentToOrder to use a one time nonce for payment method creation
-  // // const metadata: NoncePaymentMethodInput = {
-  // //   source: 'nonce-h301nyq2kycko8b6v6sr',
-  // //   last4: '1115',
-  // //   expiry_year: 2030,
-  // //   expiry_month: 3,
-  // // };
-  // const metadata: AppleOrGooglePayInput = {
-  //   source: 'googlepay',
-  //   amount: 10.8,
-  //   token:
-  //     '{"signature":"MEUCIFZG/zqpZQohvMILpMEPRC/HzlYsUvJVTlcjuh6ddNZhAiEAyRldHj7sC9xbnCa00u8dUzwRYMOoENTQqm6tyldRcDU\\u003d","intermediateSigningKey":{"signedKey":"{\\"keyValue\\":\\"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEscxfstKIwGYw8f2aybtHUYjaEKXpXh4c4uEN3NC0kZnawXUpGew8DWdS+vYOVZ5O639ZkRL32L5FeKcb0Wvdlg\\\\u003d\\\\u003d\\",\\"keyExpiration\\":\\"1743759806251\\"}","signatures":["MEYCIQCWPi+p9C/kgQMIuIUeNKRPxPWMORGO7xlm9NrMa6tfbAIhAPyJkxEIPP/ECk3U766OyHvhP5jfAk3/F2giIlNVUHIV"]},"protocolVersion":"ECv2","signedMessage":"{\\"encryptedMessage\\":\\"rXjz4P5ugdTumQ0s3B+kj7PcRDAkxJghyP/flpbxqTz40g4zaYAA9QrG+nX/bJg86I7j6NQU+cJLGTirN196p0bVCqOotx46DeOHXqDG5W2QFabxKa3igcxZ0bbQ3+OQxPN52Vz9jR1lNZkA+ZF4sckK4rtPJJltxxswVCgZm+YNUtTNb7hBh7eoJcu0WJCUv0obNcufJEz8+KoQJiaPu4TwmaIT9RanqKqlHzarXQdOhmHQJiOXfGiP8GeMaROuL9E2h8PowEyjbHEKrx0KjtRW2Jlk2wEQW4GIhietVOvO/9JpesepWX8vfn/HuVsOmXsj9BwBtKEX3Z5r9XlnMWzd8q8U8/1WUPZZgn8Q0B5vdOdzn7srmo4lFKrRBZTeyLjMZGdyINHh5ZSajWZmI9NeucEk3ZvIxAW0bV/mD5OOFmQOetclyldcEMxwsXO7E0TSp65GMAobGwNHkOd0wCN2DMyBxT4Vh4hN0dfZD4lDU2qUWjEc+/GPl/++g+s3rUR1KjyPSH06wv3yqmSG+u/0Gof/mfPoZyf1TRm6UzcFTY8HmoPeR3Oo\\",\\"ephemeralPublicKey\\":\\"BKgk4yeCEVqfZTHIAegqcnABhcc9x4v8IRQZbO7QZMEWAKBeJ9Q/fVKZgs/Tt9WyAKkH7FBfbBTbjEdh+j+3sFc\\\\u003d\\",\\"tag\\":\\"xslhQIESD/gFKNAb/SlRwD9gg8oVMiNQaSOIHFxA5Yo\\\\u003d\\"}"}',
+  // Add payment
+  // Use this metadata in AddpaymentToOrder to use a one time nonce for payment method creation
+  // const metadata: NoncePaymentMethodInput = {
+  //   source: 'nonce-h301nyq2kycko8b6v6sr',
+  //   last4: '1115',
+  //   expiry_year: 2030,
+  //   expiry_month: 3,
   // };
+  const metadata: AppleOrGooglePayInput = {
+    source: 'googlepay',
+    amount: 10.8,
+    token:
+      '{"signature":"MEUCIFZG/zqpZQohvMILpMEPRC/HzlYsUvJVTlcjuh6ddNZhAiEAyRldHj7sC9xbnCa00u8dUzwRYMOoENTQqm6tyldRcDU\\u003d","intermediateSigningKey":{"signedKey":"{\\"keyValue\\":\\"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEscxfstKIwGYw8f2aybtHUYjaEKXpXh4c4uEN3NC0kZnawXUpGew8DWdS+vYOVZ5O639ZkRL32L5FeKcb0Wvdlg\\\\u003d\\\\u003d\\",\\"keyExpiration\\":\\"1743759806251\\"}","signatures":["MEYCIQCWPi+p9C/kgQMIuIUeNKRPxPWMORGO7xlm9NrMa6tfbAIhAPyJkxEIPP/ECk3U766OyHvhP5jfAk3/F2giIlNVUHIV"]},"protocolVersion":"ECv2","signedMessage":"{\\"encryptedMessage\\":\\"rXjz4P5ugdTumQ0s3B+kj7PcRDAkxJghyP/flpbxqTz40g4zaYAA9QrG+nX/bJg86I7j6NQU+cJLGTirN196p0bVCqOotx46DeOHXqDG5W2QFabxKa3igcxZ0bbQ3+OQxPN52Vz9jR1lNZkA+ZF4sckK4rtPJJltxxswVCgZm+YNUtTNb7hBh7eoJcu0WJCUv0obNcufJEz8+KoQJiaPu4TwmaIT9RanqKqlHzarXQdOhmHQJiOXfGiP8GeMaROuL9E2h8PowEyjbHEKrx0KjtRW2Jlk2wEQW4GIhietVOvO/9JpesepWX8vfn/HuVsOmXsj9BwBtKEX3Z5r9XlnMWzd8q8U8/1WUPZZgn8Q0B5vdOdzn7srmo4lFKrRBZTeyLjMZGdyINHh5ZSajWZmI9NeucEk3ZvIxAW0bV/mD5OOFmQOetclyldcEMxwsXO7E0TSp65GMAobGwNHkOd0wCN2DMyBxT4Vh4hN0dfZD4lDU2qUWjEc+/GPl/++g+s3rUR1KjyPSH06wv3yqmSG+u/0Gof/mfPoZyf1TRm6UzcFTY8HmoPeR3Oo\\",\\"ephemeralPublicKey\\":\\"BKgk4yeCEVqfZTHIAegqcnABhcc9x4v8IRQZbO7QZMEWAKBeJ9Q/fVKZgs/Tt9WyAKkH7FBfbBTbjEdh+j+3sFc\\\\u003d\\",\\"tag\\":\\"xslhQIESD/gFKNAb/SlRwD9gg8oVMiNQaSOIHFxA5Yo\\\\u003d\\"}"}',
+  };
 
-  // try {
-  //   const { addPaymentToOrder } = await shopClient.query(ADD_PAYMENT_TO_ORDER, {
-  //     input: {
-  //       method: 'accept-blue',
-  //       // metadata,
-  //       metadata: { paymentMethodId: 14556 }, // Use a saved payment method
-  //     },
-  //   });
-  //   console.log(JSON.stringify);
-  //   console.log(
-  //     `Successfully transitioned order to ${addPaymentToOrder.state}`
-  //   );
+  try {
+    const { addPaymentToOrder } = await shopClient.query(ADD_PAYMENT_TO_ORDER, {
+      input: {
+        method: 'accept-blue',
+        // metadata,
+        metadata: { paymentMethodId: 14556 }, // Use a saved payment method
+      },
+    });
+    console.log(JSON.stringify);
+    console.log(
+      `Successfully transitioned order to ${addPaymentToOrder.state}`
+    );
 
-  //   //   // Attempt a refund
-  //   //   // const { refundAcceptBlueTransaction } = await shopClient.query(
-  //   //   //   REFUND_TRANSACTION,
-  //   //   //   {
-  //   //   //     transactionId: 354653,
-  //   //   //   }
-  //   //   // );
-  //   //   // console.log(`Refunded transaction: ${refundAcceptBlueTransaction}`);
-  // } catch (e) {
-  //   // Catch to prevent server from terminating
-  //   console.error(e);
-  // }
+    //   // Attempt a refund
+    //   // const { refundAcceptBlueTransaction } = await shopClient.query(
+    //   //   REFUND_TRANSACTION,
+    //   //   {
+    //   //     transactionId: 354653,
+    //   //   }
+    //   // );
+    //   // console.log(`Refunded transaction: ${refundAcceptBlueTransaction}`);
+  } catch (e) {
+    // Catch to prevent server from terminating
+    console.error(e);
+  }
 })();
