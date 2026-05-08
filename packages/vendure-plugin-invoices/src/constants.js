75b46f224fd399f1e1b3db719a22d2c4146040c2
Surafel <surafel@someone-2380.local>
2026-05-08 12:45:53 -0500

    feat: deleted all js files under the invocies src folder

--- packages/vendure-plugin-invoices/src/constants.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.PLUGIN_NAME = exports.PLUGIN_INIT_OPTIONS = exports.loggerCtx = void 0;
-exports.loggerCtx = 'InvoicePlugin';
-exports.PLUGIN_INIT_OPTIONS = Symbol('INVOICE_PLUGIN_OPTIONS');
-exports.PLUGIN_NAME = 'vendure-plugin-invoices';
