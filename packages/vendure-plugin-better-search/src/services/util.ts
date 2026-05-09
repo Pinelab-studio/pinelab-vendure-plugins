ce7cbdbb9020f2438dcadff44051718f3e435fe2
Martijn <martijn.brug@gmail.com>
2026-05-09 10:12:13 +0200

    feat(better-search): docs unicity

--- packages/vendure-plugin-better-search/src/services/util.ts
+++ packages/vendure-plugin-better-search/src/services/util.ts
@@ -1,7 +1,7 @@
 import { RequestContext } from '@vendure/core';
 
 /**
- * Create a unique key for the index based on the channel and language.
+ * channelToken + languageCode for indentifying indices
  */
 export function createIndexKey(ctx: RequestContext): string {
   return `${ctx.channel.token}-${ctx.languageCode}`;
