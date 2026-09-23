9ed4831eff1c1d4002f4aee8687613f56d67c8a0
Martijn <martijn@pinelab.studio>
2026-09-23 16:31:15 +0200

    fix(utm-tracker): build dashboard assets

--- packages/vendure-plugin-utm-tracker/src/gql/graphql.ts
+++ packages/vendure-plugin-utm-tracker/src/gql/graphql.ts
@@ -2,13 +2,13 @@ import type { introspection } from './graphql-env.d.ts';
 import { initGraphQLTada } from 'gql.tada';
 
 export const graphql = initGraphQLTada<{
-    disableMasking: true;
-    introspection: introspection;
-    scalars: {
-        DateTime: string;
-        JSON: any;
-        Money: number;
-    };
+  disableMasking: true;
+  introspection: introspection;
+  scalars: {
+    DateTime: string;
+    JSON: any;
+    Money: number;
+  };
 }>();
 
 export type { FragmentOf, ResultOf, VariablesOf } from 'gql.tada';
